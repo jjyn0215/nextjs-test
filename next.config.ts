24744dcf1a084a65d833bed3b070ec93b9cc9862
JHSLTP <jjyn0215@gmail.com>
2025-07-31 05:19:26 +0900

    22

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
